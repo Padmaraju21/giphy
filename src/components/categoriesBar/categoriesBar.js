65a5c3503da94a0c9ffc7e58dfe1da962bd47559
pencil <penciljamnoreply@gmail.com>
2021-05-19 21:02:16 +0100

    Categories Bar and Home Page with gifs (predefined)

--- src/components/categoriesBar/categoriesBar.js
+++ src/components/categoriesBar/categoriesBar.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import"./_categoriesBar.sass";
-function categoriesBar() {
-    return (
-        <div>
-          Categories Bar  
-        </div>
-    )
+import React, { useState } from 'react' 
+import"./_categoriesBar.scss";
+
+//keywords array for prefix searching 
+const keywords = [
+  'sad','happy',
+  'funny','wow',
+  'youre welcome',
+  'lazy','stressed',
+  'embarrassed','awesome',
+  'clapping','high five',
+]
+function CategoriesBar() {
+
+  const [activeElement,setActiveElement] = useState('All')
+  
+  const handleClick = value => {
+    setActiveElement(value)
+ }
+
+
+ return (
+  <div className='categoriesBar'>
+     {keywords.map((value, i) => (
+        <span
+           onClick={() => handleClick(value)}
+           key={i}
+           className={activeElement === value ? 'active' : ''}>
+           {value}
+        </span>
+     ))}
+  </div>
+)
 }
 
-export default categoriesBar
+export default CategoriesBar
--- src/components/gifs/Gifs.js
+++ src/components/gifs/Gifs.js
@@ -3,8 +3,11 @@ import "./_gifs.scss";
 
 const gifs = () => {
     return (
-        <div>
-           Gifs 
+        <div className="gifs">
+            <div className="gifs_top">
+                <img src='https://i.ytimg.com/an_webp/mvImjwCR3Zs/mqdefault_6s.webp?du=3000&sqp=CNeslYUG&rs=AOn4CLAi8DP8REQ7iM4UhYhE74_pxMzyew' alt = ''/>
+            </div>
+           
         </div>
     )
 }
