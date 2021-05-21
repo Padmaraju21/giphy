a940757ad5755ac22feff565d317fd7323ae593a
pencil <penciljamnoreply@gmail.com>
2021-05-21 10:24:25 +0100

    creating download functionality

--- src/screens/homeScreen/HomeScreen.jsx
+++ src/screens/homeScreen/HomeScreen.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios'
 import {useEffect} from 'react'
 
+import Download from './Download.svg'
+
 export default function Gif() {
     // STATES
     const [data, setData] = React.useState([]);
@@ -15,8 +17,7 @@ export default function Gif() {
     const [trendSearch, setTrendSearch] = React.useState(false)
     const [tSearch, setTsearch] = React.useState([])
 
-//
-// ─── FETCHING THE DATA ──────────────────────────────────────────────────────────────────────
+/**********************FETCHING DATA********************************/
 const fetchData = async (title)=>{
 let URL = `https://api.giphy.com/v1/gifs/search?q=${title}&api_key=${process.env.REACT_APP_GIFS_API_KEY}&limit=${limit}&offset=${offset}`;
 // Try and catch
@@ -57,3 +58,55 @@ useEffect(()=>{
         }
     },[offset])
 }
+/***************************HANDLING DOWNLOAD***************************** */
+const handleDownload = (url)=>{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function(){
+        let urlCreator = window.URL || window.webkitURL;
+        let imageUrl = urlCreator.createObjectURL(this.response);
+        let tag = document.createElement('a');
+        tag.href = imageUrl;
+        tag.download = title.charAt(0).toUpperCase() + title.slice(1);
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+    }
+    xhr.send();
+}
+/************************RENDERING CONTENT*************************/
+const content = () => {
+    switch(true) {
+    // If loader is true show loader spinner
+      case loader:
+        return <div>Loading...</div>
+    // If data array more than zero loop through
+      case data.length > 0:
+        return  data.map(g=> {
+            return (
+                <div className='gif-card' key={g.id}>
+                <details>
+                <summary>Show</summary>
+                    <h4>{g.title !== undefined ? (g.title.charAt(0).toUpperCase() + g.title.slice(1)) : ''}</h4>
+                <button onClick= {()=> handleDownload(g.images.fixed_height.url)} className="gif-download">
+                    <img className='svg' src={Download} alt="download"/>
+                </button>
+                   
+                </details>
+                <img onClick= {()=> handleDownload(g.images.fixed_height.url)} className='image' src={g.images.fixed_width.url} alt="gif"/>
+                </div>
+            )
+        })
+
+    // Otherwise return default
+      default:
+        return data
+    }
+  }
+
+  return (
+      <div>
+          Gify Exploer
+      </div>
+  )
